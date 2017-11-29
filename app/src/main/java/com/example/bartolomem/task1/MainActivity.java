022dfde467faea5e5cca60fa858464c5e49c1b21
BartolomeM <bartolome.mejia@fox.com>
2017-11-29 12:34:51 -0600

    otro commit

---
+++ app/src/main/java/com/example/bartolomem/task1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.bartolomem.task1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Forzar el logo, en todas las versiones android
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Instanciamos los elementos de la UI con sus referencias
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        btnNext = (Button) findViewById(R.id.buttonToSecondActivity);
+
+        btnNext.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString();
+
+                if(name != null && !name.isEmpty()){
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "The name is mandatory, plase type it", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}

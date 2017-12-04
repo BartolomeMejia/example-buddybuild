9daa04ce0d4997c00395abeb9d2b3badde4d95d3
BartolomeM <bartolome.mejia@fox.com>
2017-12-04 15:22:39 -0600

    Prueba test

---
+++ app/src/main/java/com/example/bartolomem/task1/PruebaTest.java
@@ -0,0 +1,13 @@
+package com.example.bartolomem.task1;
+
+/**
+ * Created by BartolomeM on 4/12/2017.
+ */
+
+public class PruebaTest {
+
+    public String helloWorld() {
+        return"Hola Mundo";
+    }
+
+}
---
+++ app/src/test/java/com/example/bartolomem/task1/PruebaTestTest.java
@@ -0,0 +1,20 @@
+package com.example.bartolomem.task1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by BartolomeM on 4/12/2017.
+ */
+public class PruebaTestTest {
+
+    private PruebaTest testText = new PruebaTest();
+
+    @Test
+    public void helloWorld() {
+        Assert.assertEquals("Hola Mundo", testText.helloWorld());
+    }
+
+}
